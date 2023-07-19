d2390feae7fd61b445828ea3cd022596cd7593bc
jacobm2000 <maurerja2000@gmail.com>
2023-07-19 08:43:00 -0400

    added max sentences functionality
    made it so users can input the number of sentences they want for their summary

--- flask-main.py
+++ flask-main.py
@@ -6,8 +6,9 @@ app=Flask(__name__)
 @app.route("/",methods=["PoST","GET"])
 def home() :
     if request.method=="POST":
-        s=(getSummary( str(request.form["t"])))
-        return render_template("home.html",summary=s)
+        s= str(request.form["s"])
+        summ=(getSummary( str(request.form["t"]),s))
+        return render_template("home.html",summary=summ)
     else:
         return render_template("home.html")
 if __name__== "__main__":
--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 import wikipedia
 
 
-def getSummary(term):
+def getSummary(term,sent):
+    if (sent.isnumeric()==False):
+        return "The number of sentances needs to be an even integer. Please try again"
     try:    
-        result = wikipedia.summary("'"+term+"'",sentences = 3)
+        result = wikipedia.summary("'"+term+"'",sentences = sent)
     
     except Exception as e: 
        result=e
