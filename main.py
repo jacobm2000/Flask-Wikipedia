857c3f860a9db562b236c1a3608031dd57461b53
jacobm2000 <maurerja2000@gmail.com>
2023-07-19 20:47:37 -0400

    Added error message if term input is empty

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import wikipedia
 
 
 def getSummary(term,sent):
-    if (sent.isnumeric()==False):
+    
+    if(term==""):
+        return "No term inputed. Please enter a term."
+    elif (sent.isnumeric()==False):
         return "The number of sentances needs to be an even integer. Please try again"
     try:    
         result = wikipedia.summary("'"+term+"'",sentences = sent)
